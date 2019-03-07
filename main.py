4b478f41ad222088269a0e81820eef07583ec93f
Aleksej <samarin113@gmail.com>
2019-03-07 22:59:39 +0300

    First version

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
     socket_desc.connect(('127.0.0.1', 1234))
     while True:
         try:
-            request = input('Request: ').encode()
+            request = input('Request (ANSI): ').encode('ansi')
             socket_desc.send(request)
             response = socket_desc.recv(1024)
-            print(response.decode())
+            print(f"Response (ASCII): {response.decode('ascii')}")
         except KeyboardInterrupt:
             break
     socket_desc.close()
