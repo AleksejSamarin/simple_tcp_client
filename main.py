c62c329936e8245ac1f97613b13bd9573f5c8424
Aleksej <samarin113@gmail.com>
2019-03-07 23:27:13 +0300

    Comments added

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
     socket_desc.connect(('127.0.0.1', 1234))
     while True:
         try:
-            request = input('Request (ANSI): ').encode('ansi')
+            request = input('Request (ANSI): ').encode('ansi') # формирование сообщения для сервера с кодировкой ANSI
             socket_desc.send(request)
             response = socket_desc.recv(1024)
-            print(f"Response (ASCII): {response.decode('ascii')}")
+            print(f"Response (ASCII): {response.decode('ascii')}") # принятие сообщения от сервера с кодировкой ASCII
         except KeyboardInterrupt:
             break
     socket_desc.close()
